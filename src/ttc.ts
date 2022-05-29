624d85c9da00f8ac80d6c1085024dcd576bea522
sunney-x <sanishubitidze@live.com>
2022-05-29 16:16:59 +0400

    Initial commit

---
+++ src/ttc.ts
@@ -0,0 +1,103 @@
+import {
+  ArrivalInfo,
+  ArrivalTimesResponse,
+  BusesResponse,
+  BusLiveInfo,
+  BusRoute,
+  Route,
+  RouteInfo,
+  Stop,
+  StopInfo,
+  StopRoute,
+} from "./types";
+import fetch from "node-fetch";
+
+const fetchJSON = async <T>(endpoint: string) =>
+  fetch("http://transfer.ttc.com.ge:8080/otp/routers/ttc" + endpoint).then(
+    (res) => res.json() as unknown as T
+  );
+
+const ttc = {
+  /**
+   * Get all routes.
+   * @returns {Promise<Route[]>}
+   */
+  routes: (): Promise<Route[]> => fetchJSON("/routes"),
+
+  /**
+   * Get all bus stops
+   * @returns {Promise<Stop[]>}
+   */
+  busRoutes: (): Promise<BusRoute[]> => fetchJSON("/routes?type=3"),
+
+  /**
+   * Returns a list of stops, note that it includes stops for metro as well.
+   * @returns {Promise<Stop[]>}
+   */
+  stops: (): Promise<Stop[]> => fetchJSON("/index/stops"),
+
+  /**
+   * Bus/Metro stop info
+   * @param stopId The stop id, e.g. "1:1946"
+   * @returns {Promise<StopInfo>}
+   */
+  stopInfo: (stopId: string): Promise<StopInfo> =>
+    fetchJSON(`/index/stops/${stopId}`),
+
+  /**
+   * Get all routes for a stop
+   * @param stopId The stop id, e.g. "1:1946"
+   * @returns {Promise<StopRoute[]>}
+   */
+  stopRoutes: (stopId: string): Promise<StopRoute[]> =>
+    fetchJSON(`/index/stops/${stopId}/routes`),
+
+  /**
+   * Get all arrivals for a stop
+   * @example
+   *
+   * ```
+   * ttc.stopArrivalTimes("1946").then((arrivalTimes) => {
+   *   for (const at of arrivalTimes) {
+   *     console.log(at.ArrivalTime, at.RouteNumber);
+   *   }
+   * });
+   * ```
+   *
+   * @param stopId The stop id, e.g. "1946"
+   * @returns {Promise<ArrivalTimes>}
+   * @see https://transfer.ttc.com.ge/otp/routers/ttc/index/stops/1946/arrivals
+   */
+  stopArrivalTimes: (stopId: string): Promise<ArrivalInfo[]> =>
+    fetchJSON<ArrivalTimesResponse>(`/stopArrivalTimes?stopId=${stopId}`).then(
+      (res) => res.ArrivalTime
+    ),
+
+  /**
+   * @param routeNumber The bus number
+   * @param forward If true, returns the stops for the route in the forward direction.
+   * @returns {Promise<RouteInfo>}
+   */
+  busRouteInfo: (routeNumber: string, forward: boolean): Promise<RouteInfo> =>
+    fetchJSON(
+      `/routeInfo?routeNumber=${routeNumber}&type=bus&forward=${Number(
+        forward
+      )}`
+    ),
+
+  /**
+   * Get live info about buses
+   * @param routeNumber The bus number
+   * @param forward If true, returns the stops for the route in the forward direction.
+   * @returns {Promise<BusLiveInfo>}
+   */
+  busLiveLocations: (
+    routeNumber: string,
+    forward: boolean
+  ): Promise<BusLiveInfo[]> =>
+    fetchJSON<BusesResponse>(
+      `/buses?routeNumber=${routeNumber}&forward=${Number(forward)}`
+    ).then((res) => res.bus),
+};
+
+export default ttc;
---
+++ src/types.ts
@@ -0,0 +1,113 @@
+export enum Color {
+  Ff505B = "ff505b",
+  The117A65 = "117a65",
+  The1F618D = "1f618d",
+}
+
+export enum Mode {
+  Bus = "BUS",
+  Subway = "SUBWAY",
+}
+
+export interface Route {
+  Id: string;
+  Type: TransportType;
+  RouteNumber: string;
+  Color: Color;
+  StopA: string;
+  StopB: string;
+  RouteStops: any[];
+  LongName?: string;
+}
+
+export type BusRoute = Route & {
+  Type: TransportType.Bus;
+};
+
+export enum TransportType {
+  Bus = "bus",
+  Metro = "metro",
+}
+
+export interface Stop {
+  id: string;
+  code?: string;
+  name: string;
+  lat: number;
+  lon: number;
+  cluster?: string;
+}
+
+export interface StopInfo {
+  id: string;
+  name: string;
+  lat: number;
+  lon: number;
+  code: string;
+  locationType: number;
+  wheelchairBoarding: number;
+  vehicleType: number;
+  vehicleTypeSet: boolean;
+}
+
+export interface StopRoute {
+  id: string;
+  shortName: string;
+  longName?: string;
+  mode: Mode;
+  color: Color;
+  agencyName: "თბილისის სატრანსპორტო კომპანია";
+}
+
+export interface RouteInfo {
+  Id: string;
+  Type: string;
+  RouteNumber: string;
+  LongName: string;
+  Color: string;
+  Shape: string;
+  RouteStops: RouteStop[];
+}
+
+export interface RouteStop {
+  StopId: string;
+  Name: string;
+  Forward: boolean;
+  Lon: number;
+  Lat: number;
+  Sequence: number;
+  Type: TransportType;
+  HasBoard: boolean;
+  Virtual: boolean;
+  Routes: any[];
+}
+
+export interface ArrivalTimesResponse {
+  ArrivalTime: ArrivalInfo[];
+}
+
+export interface ArrivalInfo {
+  RouteNumber: string;
+  DestinationStopName: string;
+  ArrivalTime: number;
+}
+
+export interface BusesResponse {
+  routeNumber: string;
+  forward: boolean;
+  lon: number;
+  lat: number;
+  nextStopId: string;
+}
+
+export interface BusesResponse {
+  bus: BusLiveInfo[];
+}
+
+export interface BusLiveInfo {
+  routeNumber: string;
+  forward: boolean;
+  lon: number;
+  lat: number;
+  nextStopId: string;
+}
